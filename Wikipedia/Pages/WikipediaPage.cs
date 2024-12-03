84e1c4a26b5ebdb9858d29b6c5dd5a3225738849
Branislav Kotrc <BKotrc@moravia.com>
2024-12-03 13:53:47 +0100

    Add project files.

---
+++ Wikipedia/Pages/WikipediaPage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Playwright;
+
+namespace Wikipedia.Pages;
+
+public class WikipediaPage
+{
+    private readonly IPage _page;
+
+    public WikipediaPage(IPage page)
+    {
+        _page = page;
+    }
+
+    private ILocator FirstLink
+        => _page.Locator("(//div[contains(@class, 'mw-content-ltr')]/p//a[starts-with(@href, '/wiki/')])[1]");
+
+    public async Task NavigateToRandomPageAsync()
+    {
+        await _page.GotoAsync("https://en.wikipedia.org/wiki/Special:Random");
+    }
+
+    public async Task<string> GetCurrentUrlAsync()
+    {
+        return _page.Url;
+    }
+
+    public async Task ClickFirstLinkAsync()
+    {
+        Console.WriteLine($"Redirecting to: {await FirstLink.GetAttributeAsync("href")}");
+        await FirstLink.ClickAsync();
+    }
+}
---
+++ Wikipedia/Tests/WikipediaTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using Wikipedia.Pages;
+
+namespace Wikipedia.Tests;
+
+[TestFixture]
+public class WikipediaTest
+{
+    private IBrowser? _browser;
+    private IPage? _page;
+
+    [OneTimeSetUp]
+    public async Task SetupAsync()
+    {
+        var playwright = await Playwright.CreateAsync();
+        _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+        _page = await _browser.NewPageAsync();
+    }
+
+    [Test]
+    public async Task NavigateToPhilosophyTest()
+    {
+        if (_page == null) throw new InvalidOperationException("Page is not initialized.");
+
+        var wikipediaPage = new WikipediaPage(_page);
+        await wikipediaPage.NavigateToRandomPageAsync();
+
+        Console.WriteLine($"Starting page: {await wikipediaPage.GetCurrentUrlAsync()}");
+        List<string> urls = new List<string> {await wikipediaPage.GetCurrentUrlAsync()};
+
+        var redirects = 0;
+
+        while (!(await wikipediaPage.GetCurrentUrlAsync()).Split('/').Last().Equals("Philosophy", StringComparison.OrdinalIgnoreCase))
+        {
+            await wikipediaPage.ClickFirstLinkAsync();
+
+            if (urls.Contains(await wikipediaPage.GetCurrentUrlAsync()))
+            {
+                Assert.Fail("Navigation stopped because this page was already visited");
+                break;
+            }
+
+            urls.Add(await wikipediaPage.GetCurrentUrlAsync());
+
+            redirects++;
+        }
+
+        Assert.That((await wikipediaPage.GetCurrentUrlAsync()).Contains("Philosophy"), Is.True, $"Failed to reach 'Philosophy' after {redirects} redirects.");
+        Console.WriteLine($"Reached 'Philosophy' after {redirects} redirects.");
+    }
+
+    [OneTimeTearDown]
+    public async Task TeardownAsync()
+    {
+        if (_browser != null)
+        {
+            await _browser.CloseAsync();
+        }
+    }
+}
